171f8553f72d24b8b2b707371a1b5c93fbfdb8d6
swethanaagu <vishwakarmanagu@gmail.com>
2019-12-19 16:54:42 +0530

    write show metho in student controller

--- SP_SC_Hibernate/src/main/java/com/vktechnology/naagu/controller/StudentController.java
+++ SP_SC_Hibernate/src/main/java/com/vktechnology/naagu/controller/StudentController.java
@@ -1,5 +1,21 @@
 package com.vktechnology.naagu.controller;
 
-public class StudentController {
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
+
+@Controller
+public class StudentController{
+	
+	private static final Logger logger = Logger.getLogger(StudentController.class);
+	
+	@RequestMapping(value = "/openStudentResult", method = RequestMethod.GET)
+    public String credit() {
+        logger.info("-------------++ student ++------------");
+        String showpath = "redirect:/htmlpages/student.html";
+        return showpath;
+    }
 
 }

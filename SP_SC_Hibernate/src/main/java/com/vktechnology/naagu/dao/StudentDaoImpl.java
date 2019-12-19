b8cb01325ae65b7144101a5eac569b89ba7e6af2
swethanaagu <vishwakarmanagu@gmail.com>
2019-12-19 23:49:29 +0530

    write student save code

--- SP_SC_Hibernate/src/main/java/com/vktechnology/naagu/dao/StudentDaoImpl.java
+++ SP_SC_Hibernate/src/main/java/com/vktechnology/naagu/dao/StudentDaoImpl.java
@@ -1,12 +1,15 @@
 package com.vktechnology.naagu.dao;
 
+import javax.transaction.Transactional;
+
+import org.apache.log4j.Logger;
 import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 
 import com.vktechnology.naagu.models.Student;
 
 public class StudentDaoImpl implements StudentDao{
+	
+	private static final Logger logger = Logger.getLogger(StudentDaoImpl.class);
 	
 	private SessionFactory sessionFactory;
 	
@@ -14,9 +17,17 @@ public class StudentDaoImpl implements StudentDao{
 		this.sessionFactory = sessionFactory;
 	}
 	
+	{System.out.println("call constr");}
+	
+	public void setSessionFactory(SessionFactory sessionFactory){
+		this.sessionFactory = sessionFactory;
+	}
+	
 	@Override
+	@Transactional
 	public String addStudent(Student student){
-		
-		return "nagaraju";
+		logger.info("------------inside--------------");
+		long a = (Long) sessionFactory.getCurrentSession().save(student);
+		return a+"";
 	}
 }
